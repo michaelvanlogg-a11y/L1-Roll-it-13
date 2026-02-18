83498e2d606c6036599c50b44b73a1325231884a
michaelvanlogg-a11y <michael.vanlogg@papamoacollege.school.nz>
2026-02-19 10:54:11 +1300

    Add files via upload

---
+++ C_01_yes_no_basic.py
@@ -0,0 +1,11 @@
+want_instructions =input("Do you want to see the instructions? ")
+
+# check the user says yes / no
+if want_instructions == "yes":
+    print("you said yes")
+elif want_instructions == "no":
+    print("you said no")
+else:
+    print("please enter yes / no")
+
+    
